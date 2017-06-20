346459a13efcf184ce10810fefee86e066cf8c38
Kyromyr <Chyromyr@gmail.com>
2017-06-20 23:25:25 +0200

    Now shows perfect gems. Changed how excludes are checked.

--- Lua/excludes.lua
+++ Lua/excludes.lua
@@ -3,15 +3,17 @@ local f = io.open("items.txt", "rb");
 local gemTypes = {Amethyst=1, Topaz=1, Sapphire=1, Emerald=1, Ruby=1, Diamond=1, Skull=1, Onyx=1, Bloodstone=1, Turquoise=1, Amber=1, ["Rainbow Stone"]=1};
 local gemGrades = {Chipped=1,Flawed=2,[""]=3,Flawless=4,Perfect=5};
 
-local itemType, code, hex, name, rune, pot, gemGrade, gemType;
+local hide, itemType, code, name, rune, pot, gemGrade, gemType;
 
 local readable = {};
 local output = {};
+local class = 0;
 
 for line in f:lines() do
 	code, name = line:match"%[.?(....)%] <(.*)>";
 	if (code and name) then
-		hex = string.format("'%s'", code);
+		hide = 0;
+		
 		rune = tonumber(code:match"r(%d%d) ");
 		pot = tonumber(code:match"hp(%d) ");
 
@@ -23,16 +25,19 @@ for line in f:lines() do
 		gemGrade = gemGrades[gemGrade];
 		
 		if ((itemType ~= "misc" and name:match"%(%d%)$") -- Non-sacred equipment
-			or (rune and rune < 34) -- cLOD runes
-			or (gemTypes[gemType] and gemGrade and gemGrade < 6) -- All gems
+			or (rune and rune < 34) -- cLOD runes (Zod and below)
+			or (gemTypes[gemType] and gemGrade and gemGrade < 5) -- Gems below perfect
 			or (pot and pot < 4) -- Health potions below Greater
 			or code:match"mp%d " -- Mana potions
 			or code == "tsc " or code == "isc " or code == "key " or code == "tbk " or code == "ibk " -- TP/ID scrolls and tomes, keys
 			or code == "ey2 " or code == "ey3 " or code == "et1 " -- Elixirs of Experience, Greed & Concentration
 			) then
 			table.insert(readable, line);
-			table.insert(output, hex);
+			hide = 1;
 		end
+		
+		table.insert(output, hide);
+		class = class + 1;
 	else
 		code = line:match"{(%w+)}";
 		itemType = code or itemType;
@@ -40,14 +45,14 @@ for line in f:lines() do
 end
 f:close();
 
-f = io.open("output-readable.txt", "w+b");
+f = io.open("excludes-readable.txt", "w+b");
 f:setvbuf"no";
 f:write(table.concat(readable, "\r\n"));
 f:flush();
 f:close();
 
-f = io.open("output.txt", "w+b");
+f = io.open("excludes.cpp", "w+b");
 f:setvbuf"no";
-f:write("DWORD excludes[] = { ", table.concat(output, ","), " };");
+f:write("char excludes[] = { ", table.concat(output, ","), " };");
 f:flush();
 f:close();
